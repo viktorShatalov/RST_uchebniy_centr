b6060eee05720fdfa5d122f9b2a760f996485d72
viktr 666 <49025707+viktr1432@users.noreply.github.com>
2020-06-17 01:08:16 +0300

    ++

--- js/main.js
+++ js/main.js
@@ -15,4 +15,41 @@ jQuery(document).ready(function () {
         jQuery('.dropdown__hide').toggleClass('active')
     });
 
+    // magnific-popup
+    // jQuery(document).ready(function () {
+    //     jQuery('.popup-gallery').magnificPopup({
+    //         delegate: 'a',
+    //         type: 'image',
+    //         tLoading: 'Loading image #%curr%...',
+    //         mainClass: 'mfp-with-zoom',
+
+    //         zoom: {
+    //             enabled: true, // By default it's false, so don't forget to enable it
+
+    //             duration: 300, // duration of the effect, in milliseconds
+    //             easing: 'ease-in-out', // CSS transition easing function
+
+    //             // The "opener" function should return the element from which popup will be zoomed in
+    //             // and to which popup will be scaled down
+    //             // By defailt it looks for an image tag:
+    //             opener: function (openerElement) {
+    //                 // openerElement is the element on which popup was initialized, in this case its <a> tag
+    //                 // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+    //                 return openerElement.is('img') ? openerElement : openerElement.find('img');
+    //             }
+    //         },
+    //         gallery: {
+    //             enabled: true,
+    //             navigateByImgClick: true,
+    //             preload: [0, 1]
+    //         },
+    //         image: {
+    //             tError: '<a href="%url%">Картинка #%curr%</a> не может загрузиться.',
+    //             titleSrc: function (item) {
+    //                 return item.el.attr('title') + '<small>Ждём-с...</small>';
+    //             }
+    //         }
+    //     });
+    // });
+
 })
